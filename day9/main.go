9c5939e5b98d43525fa8ae49b1ea16bc21928fb3
Elijah Morgan <elijahjmorgan@gmail.com>
2024-12-23 19:35:57 -0500

    day 9 pt 2

--- day9/main.go
+++ day9/main.go
@@ -20,7 +20,7 @@ func part1(puzzle []int) int {
 }
 
 func part2(puzzle []int) int {
-	return 0
+	return solve2(puzzle)
 }
 
 func readInput() []int {
@@ -64,6 +64,16 @@ func solve(puzzle []int) int {
 	return countMemory(mem)
 }
 
+func solve2(puzzle []int) int {
+	sum := calcSize(puzzle)
+	mem := make([]int, sum)
+	fillMemory(mem, puzzle)
+	// fmt.Println(mem)
+	sortMemory2(mem)
+	// fmt.Println(mem)
+	return countMemory(mem)
+}
+
 func sortMemory(mem []int) {
 	var last int
 
@@ -85,11 +95,82 @@ func sortMemory(mem []int) {
 	}
 }
 
+func sortMemory2(mem []int) {
+	var last int
+	last = len(mem) - 1
+	// find file
+
+	for last > 0 {
+		var size int
+		// pass in mem and last and then update last
+		// last is the end of the file, so base is last - size
+		last, size = findFile(mem, last)
+		base := last - size + 1
+		last = base - 1
+		if last <= 0 || size == 0 {
+			return
+		}
+
+		// fmt.Println(last, size)
+		start := findSpace(mem, size) // look for space anywhere
+		if start == -1 {
+			// no space
+			continue
+		}
+		if start > base {
+			continue // can't swap to a later place
+		}
+
+		// swap
+		for i := 0; i < size; i++ {
+			mem[start+i], mem[base+i] = mem[base+i], mem[start+i]
+		}
+		// fmt.Println(mem)
+
+		// we are done
+	}
+}
+
+func findSpace(mem []int, l int) int {
+	var start, cl int
+	for i, v := range mem {
+		if v == -1 {
+			start = i
+			cl++
+			if cl == l {
+				return start - cl + 1
+			}
+		} else {
+			cl = 0
+		}
+	}
+	return -1
+}
+
+// return start/index, size
+func findFile(mem []int, index int) (int, int) {
+	var start, size int
+
+	for j := index; j > 0; j-- {
+		lastVal := mem[j]
+		// find file start
+		if lastVal != -1 {
+			start = j
+			for ; lastVal == mem[j] && j > 0; j-- {
+				size++
+			}
+			break
+		}
+	}
+
+	return start, size
+}
+
 func countMemory(mem []int) int {
 	var sum int
 	for i, v := range mem {
 		if v == -1 {
-			break
+			continue
 		}
 		sum += i * v
 	}
